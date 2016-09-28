32db9a8de2a4966392d63b28ebfc449c2a94c353
gbanjac <goran.banjac@eng.ox.ac.uk>
2016-09-28 12:00:53 -0700

    Dual variables extracted from the solution

--- quadprog.py
+++ quadprog.py
@@ -23,10 +23,14 @@ class quadProgSolution:
 	"""
 	stores QP solution
 	"""
-	def __init__(self, status, objval, sol):
+	def __init__(self, status, objval, sol_prim, sol_dual_eq, sol_dual_ineq, sol_dual_lb, sol_dual_ub):
 		self.status = status
 		self.objval = objval
-		self.sol = sol
+		self.sol_prim = sol_prim
+		self.sol_dual_eq = sol_dual_eq
+		self.sol_dual_ineq = sol_dual_ineq
+		self.sol_dual_lb = sol_dual_lb
+		self.sol_dual_ub = sol_dual_ub
 
 
 def project(xbar, lb, ub):
@@ -42,7 +46,7 @@ def project(xbar, lb, ub):
 
 
 # Base QP Solver
-def SQPSSolve(Q, c, Aeq, beq, Aineq, bineq, lb, ub, x0=0, max_iter=500, rho=1.6):
+def SQPSSolve(Q, c, Aeq, beq, Aineq, bineq, lb, ub, x0=0, max_iter=500, rho=1.6, printiter=25):
 	"""
 	Operator splitting solver for a QP problem given
 	in the following form
@@ -58,32 +62,28 @@ def SQPSSolve(Q, c, Aeq, beq, Aineq, bineq, lb, ub, x0=0, max_iter=500, rho=1.6)
 	nineq = bineq.shape[0]
 	nvar = nx + nineq  # Number of variables in standard form: x and s variables
 	
-	# Reformat missing data
-	
-	
-	# Form complete (c) matrices for inequality constraints
-	Ac = np.vstack([np.hstack([Aeq, np.zeros((neq, nineq))]), np.hstack([Aineq, np.eye(nineq)])])
-	bc = np.append(beq, bineq)
+	# Form compact (c) matrices for standard QP from
 	Qc = spla.block_diag(Q, np.zeros((nineq, nineq)))
 	cc = np.append(c, np.zeros(nineq))
-	
-	# Factorize Matrices (Later)
-	M = np.vstack([np.hstack([Qc + rho*np.eye(nvar), Ac.T]), np.hstack([Ac, -1./rho*np.eye(nineq + neq)])])
-	
-	# Factorize matrix M using LU decomposition
-	LU, piv = spla.lu_factor(M)
+	Ac = np.vstack([np.hstack([Aeq, np.zeros((neq, nineq))]), np.hstack([Aineq, np.eye(nineq)])])
+	bc = np.append(beq, bineq)
+	  
+	# Factorize KKT matrix using LU decomposition (for now)
+	KKT = np.vstack([np.hstack([Qc + rho*np.eye(nvar), Ac.T]), np.hstack([Ac, -1./rho*np.eye(nineq + neq)])])
+	LU, piv = spla.lu_factor(KKT)
 	
 	print "Splitting QP Solver"
 	print "-------------------\n"
-	print "iter |\t %6s\n" % 'cost'
+	print "iter |\t   cost\n"
 	
 	# Run ADMM
 	z = np.zeros(nvar)
 	u = np.zeros(neq + nineq + nvar)  # Number of dual variables in ADMM
 	
 	for i in range(max_iter):
+	
+		# Update RHS of KKT system
 		qtemp = -cc + rho*(z + np.dot(Ac.T, bc) - np.dot(Ac.T, u[:neq + nineq]) - u[neq + nineq:])
-		
 		qbar = np.append(qtemp, np.zeros(nineq+neq))
 		
 		# x update
@@ -93,32 +93,43 @@ def SQPSSolve(Q, c, Aeq, beq, Aineq, bineq, lb, ub, x0=0, max_iter=500, rho=1.6)
 		# u update
 		u = u + np.append(np.dot(Ac, x), x) - np.append(np.zeros(neq + nineq), z) - np.append(bc, np.zeros(nvar))
 		
-		# Compute cost function
-		xtemp = x[:nx]
-		f = .5*np.dot(np.dot(xtemp.T, Q), xtemp) + np.dot(c.T, xtemp)
+		#todo: Stopping criterion
 		
-		if (i+1 == 1) | (np.mod(i+1, 25) == 0):
+		# Print cost function every printiter iterations
+		if (i+1 == 1) | (np.mod(i+1, printiter) == 0):
+			xtemp = z[:nx]
+			f = .5*np.dot(np.dot(xtemp.T, Q), xtemp) + np.dot(c.T, xtemp)
 			print "%4s | \t%7.2f" % (i+1, f)
-	
 	print "Optimization Done\n"
 	
-	sol = x[:nx]
-	objval = .5*np.dot(np.dot(sol, Q), sol) + np.dot(c, sol)
-	return quadProgSolution(OPTIMAL, objval, sol)
+	#todo: What is a status of the obtained solution?
+	
+	#todo: Solution polishing
+	
+	# Retrieve solution
+	sol_prim = z[:nx]
+	objval = .5*np.dot(np.dot(sol_prim, Q), sol_prim) + np.dot(c, sol_prim)
+	
+	# Retrieve dual variables
+	sol_dual_eq = rho*u[:neq]
+	sol_dual_ineq = rho*u[neq:neq+nineq]	
+	stat_cond = np.dot(Q, sol_prim) + c + np.dot(Aeq.T, sol_dual_eq) + np.dot(Aineq.T, sol_dual_ineq)
+	sol_dual_lb = -np.minimum(stat_cond, 0)
+	sol_dual_ub = np.maximum(stat_cond, 0)
+	
+	# Return solution as a quadProgSolution object
+	return quadProgSolution(OPTIMAL, objval, sol_prim, sol_dual_eq, sol_dual_ineq, sol_dual_lb, sol_dual_ub)
+
+
 
 
 def isPSD(A, tol=1e-8):
-	E, _ = sp.linalg.eigh(A)
-	return np.all(E > -tol)
+	eigs, _ = sp.linalg.eigh(A)
+	return np.all(eigs > -tol)
 
 
 
-
-
-def main(argv=[]):
-    """
-    Handle command line arguments from the operating system
-    """
+def main():
 	
 	# Select an example QP
 	expl = 'random' # {'small', 'random'}
@@ -138,43 +149,62 @@ def main(argv=[]):
 		ub = 5.*np.ones(nx)
 	elif expl == 'small':
 		nx = 2
+		neq = 1
+		nineq = 0
 		# Generate a small example
 		Q = np.array([ [4., 1.], [1., 2.] ])
-		c = np.ones(2)
-		Aeq = np.ones((1,2))
+		c = np.ones(nx)
+		Aeq = np.ones((1,nx))
 		beq = np.array([1.0])
 		Aineq = np.zeros((0,nx))
 		bineq = np.zeros(0)
-		lb = np.zeros(2)
-		ub = np.Inf*np.ones(2)
+		lb = np.zeros(nx)
+		ub = np.Inf*np.ones(nx)
 	else:
 		assert False, "Unknown example"
 	
-	# Solve QP ADMM
-	results = SQPSSolve(Q, c, Aeq, beq, Aineq, bineq, lb, ub)
+	# Solve QP via ADMM
+	solADMM = SQPSSolve(Q, c, Aeq, beq, Aineq, bineq, lb, ub)
 	
-	# Solve QP with cvxpy
+	# Solve QP via cvxpy+ECOS
 	x = cvx.Variable(nx)
+	dict_constr = {}	# dict_constr has keys of all given constraints
 	constraints = []
 	if beq.size:  # if beq is nonempty
 		constraints = constraints + [Aeq*x == beq]
+		dict_constr['eq'] = len(constraints)-1	# value assigned to each key is an index of the constraint
 	if bineq.size:
 		constraints = constraints + [Aineq*x <= bineq]
+		dict_constr['ineq'] = len(constraints)-1
 	if lb.size:
 		constraints = constraints + [x >= lb]
+		dict_constr['lb'] = len(constraints)-1
 	if ub.size:
 		constraints = constraints + [x >= lb]
+		dict_constr['ub'] = len(constraints)-1
 	objective = cvx.Minimize(.5*cvx.quad_form(x, Q) + c.T*x)
 	problem = cvx.Problem(objective, constraints)
-	resultECOS = problem.solve(solver=cvx.ECOS, verbose=False)
+	problem.solve(solver=cvx.ECOS, verbose=False)
+	# Get dual variables
+	dual_eq   = np.asarray(constraints[dict_constr['eq']].dual_value).flatten()   if 'eq'   in dict_constr.keys() else np.zeros(0)
+	dual_ineq = np.asarray(constraints[dict_constr['ineq']].dual_value).flatten() if 'ineq' in dict_constr.keys() else np.zeros(0)
+	dual_lb   = np.asarray(constraints[dict_constr['lb']].dual_value).flatten()   if 'lb'   in dict_constr.keys() else np.zeros(0)
+	dual_ub   = np.asarray(constraints[dict_constr['ub']].dual_value).flatten()   if 'ub'   in dict_constr.keys() else np.zeros(0)
 	
 	# Compare solutions
-	print "ADMM Objective Value = %.3f\n" % results.objval,
+	print "ADMM Objective Value = %.3f\n" % solADMM.objval,
 	print "ECOS Objective Value = %.3f" % objective.value
 	
-	#ipdb.set_trace()
-
-
+	# Compare dual variables of ADMM and ECOS solutions
+	if 'eq' in dict_constr.keys():
+		print "dual_eq DIFF   = %.2f" % npla.norm(dual_eq - solADMM.sol_dual_eq)
+	if 'ineq' in dict_constr.keys():
+		print "dual_ineq DIFF = %.2f" % npla.norm(dual_ineq - solADMM.sol_dual_ineq)
+	if 'lb' in dict_constr.keys():
+		print "dual_lb DIFF   = %.2f" % npla.norm(dual_lb - solADMM.sol_dual_lb)
+	if 'ub' in dict_constr.keys():
+		print "dual_ub DIFF   = %.2f" % npla.norm(dual_ub - solADMM.sol_dual_ub)
+	
 
 	
 # Parsing optional command line arguments
