4cef9f13adb89d6572d09a12dd70d14362817502
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2016-10-05 16:03:05 -0700

    Fixed Gurobi problem

--- quadprog/solvers/gurobi_qpif.py
+++ quadprog/solvers/gurobi_qpif.py
@@ -3,7 +3,7 @@ import numpy as np
 from quadprog.results import quadprogResults
 import gurobipy as grb
 import quadprog.problem as qp
-
+import ipdb
 
 class GUROBI(object):
     """
@@ -45,7 +45,7 @@ class GUROBI(object):
 
         # Add variables
         for i in range(nx):
-            m.addVar(lb=p.lb[i], ub=p.ub[i], obj=p.c[i])
+            m.addVar(lb=p.lb[i], ub=p.ub[i])
         m.update()
         x = m.getVars()
 
@@ -58,6 +58,7 @@ class GUROBI(object):
             coeff = p.Aeq.data[start:end]
             expr = grb.LinExpr(coeff, variables)
             m.addConstr(lhs=expr, sense=grb.GRB.EQUAL, rhs=p.beq[i])
+        m.update()
 
         # Add inequality constraints: iterate over the rows of Aeq
         # adding each row into the model
@@ -68,12 +69,14 @@ class GUROBI(object):
             coeff = p.Aineq.data[start:end]
             expr = grb.LinExpr(coeff, variables)
             m.addConstr(lhs=expr, sense=grb.GRB.LESS_EQUAL, rhs=p.bineq[i])
+        m.update()
 
-        # Set quadratic cost
-        obj = grb.QuadExpr()
+        # Define objective
+        obj = grb.QuadExpr()  # Set quadratic part
         for i in range(p.Q.nnz):
-            obj += p.Q.data[i]*x[p.Q.row[i]]*x[p.Q.col[i]]
-        m.setObjective(obj)
+            obj.add(.5*p.Q.data[i]*x[p.Q.row[i]]*x[p.Q.col[i]])
+        obj.add(grb.LinExpr(p.c, x))  # Add linear part
+        m.setObjective(obj)  # Set objective
 
         # Update model
         m.update()
@@ -84,19 +87,21 @@ class GUROBI(object):
         # Return results
         # Get objective value
         objval = m.objVal
+
         # Get status
         status = self.STATUS_MAP.get(m.Status, qp.SOLVER_ERROR)
+
         # Get solution
         sol = np.array([x[i].X for i in range(nx)])
 
-        # Get dual variables
+        # Get dual variables  (Gurobi uses swapped signs (-1))
         constrs = m.getConstrs()
-        sol_dual_eq = -np.array([constrs[i].Pi for i in range(neq)]) # Gurobi uses swapped signs (-1)
-        sol_dual_ineq = np.array([constrs[i+neq].Pi for i in range(nineq)])
+        sol_dual_eq = -np.array([constrs[i].Pi for i in range(neq)])
+        sol_dual_ineq = -np.array([constrs[i+neq].Pi for i in range(nineq)])
 
         # Bounds
-        sol_dual_ub = np.array(nx)
-        sol_dual_lb = np.array(nx)
+        sol_dual_ub = np.zeros(nx)
+        sol_dual_lb = np.zeros(nx)
 
         RCx = [x[i].RC for i in range(nx)]  # Get reduced costs
         for i in range(nx):
@@ -108,5 +113,9 @@ class GUROBI(object):
         # Get computation time
         cputime = m.Runtime
 
+        # Total Number of iterations
+        total_iter = m.BarIterCount
+
         return quadprogResults(status, objval, sol, sol_dual_eq,
-                               sol_dual_ineq, sol_dual_lb, sol_dual_ub, cputime)
+                               sol_dual_ineq, sol_dual_lb, sol_dual_ub,
+                               cputime, total_iter)
