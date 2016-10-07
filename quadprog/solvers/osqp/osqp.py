9b0863844b4a9ba03e2254bdc4194916cdc08f76
gbanjac <goran.banjac@eng.ox.ac.uk>
2016-10-07 11:36:31 -0700

    Problem dimensions are now part of problem class

--- quadprog/solvers/osqp/osqp.py
+++ quadprog/solvers/osqp/osqp.py
@@ -51,14 +51,19 @@ class problem(object):
     """
 
     def __init__(self, Q, c, Aeq, beq, Aineq, bineq, lb=None, ub=None):
+        # Set problem dimensions
+        self.nx = c.size
+        self.neq = beq.size
+        self.nineq = bineq.size
+        # Set problem data
         self.Q = Q
         self.c = c
         self.Aeq = Aeq
         self.beq = beq
         self.Aineq = Aineq
         self.bineq = bineq
-        self.lb = lb if lb is not None else -np.inf*np.ones(c.size)
-        self.ub = ub if ub is not None else np.inf*np.ones(c.size)
+        self.lb = lb if lb is not None else -np.inf*np.ones(self.nx)
+        self.ub = ub if ub is not None else np.inf*np.ones(self.nx)
 
 
 class options(object):
@@ -118,16 +123,29 @@ class OSQP(object):
         """
         self.problem = problem(Q, c, Aeq, beq, Aineq, bineq, lb, ub)
 
-    def project(self, xbar, lb, ub):
+    def project(self, xbar):
         """
-        Project first nx (size of lb) elements on interval [lb, ub],
-        and the rest on positive orthant.
+        Project a vector xbar onto the constraint set C.
         """
-        nx = np.size(lb)
-        # Project x part to [lb, ub] interval
-        xbar[:nx] = np.minimum(np.maximum(xbar[:nx], lb), ub)
-        # Project slack variables to positive ortant
-        xbar[nx:] = np.maximum(xbar[nx:], 0)
+        if self.options.splitting == 1:
+            # Project first nx elements on interval [lb, ub],
+            # and the rest on positive orthant.
+            xbar[:self.problem.nx] = np.minimum(
+                                        np.maximum(xbar[:self.problem.nx],
+                                        self.problem.lb), self.problem.ub)
+            xbar[self.problem.nx:] = np.maximum(xbar[self.problem.nx:], 0.0)
+        elif self.options.splitting == 2:
+            # Project first nx elements on interval [lb, ub],
+            # next neq elements on zero, and the rest on positive
+            # orthant.
+            xbar[:self.problem.nx] = np.minimum(
+                                        np.maximum(xbar[:self.problem.nx],
+                                        self.problem.lb), self.problem.ub)
+            xbar[self.problem.nx:self.problem.nx+self.problem.neq] = 0.0
+            xbar[self.problem.nx+self.problem.neq:] = np.maximum(
+                                xbar[self.problem.nx+self.problem.neq:], 0.0)
+        else:
+            assert False, "Unknown splitting"
 
         return xbar
 
@@ -140,9 +158,6 @@ class OSQP(object):
         sol_x = solution.x
         sol_lb = solution.sol_dual_lb
         sol_ub = solution.sol_dual_ub
-        nx = self.problem.c.shape[0]
-        neq = self.problem.Aeq.shape[0]
-        nineq = self.problem.Aineq.shape[0]
 
         # Try to guess from an approximate solution which bounds are active
         bool_lb = np.logical_and(sol_x < (self.problem.lb+self.problem.ub)/2.,
@@ -163,20 +178,32 @@ class OSQP(object):
         # Solve the corresponding linear system
         KKT = spspa.vstack([
             spspa.hstack([self.problem.Q, self.problem.Aeq.T,
-                         self.problem.Aineq.T, spspa.eye(nx)]),
+                         self.problem.Aineq.T, spspa.eye(self.problem.nx)]),
             spspa.hstack([self.problem.Aeq,
-                         spspa.csr_matrix((neq, neq+nineq+nx))]),
+                         spspa.csr_matrix((self.problem.neq, self.problem.neq +
+                                           self.problem.nineq +
+                                           self.problem.nx))]),
             spspa.hstack([self.problem.Aineq[ind_act],
-                          spspa.csr_matrix((len(ind_act), neq+nineq+nx))]),
-            spspa.hstack([spspa.csr_matrix((len(ind_inact), nx+neq)),
-                          spspa.eye(nineq).tocsr()[ind_inact],
-                          spspa.csr_matrix((len(ind_inact), nx))]),
-            spspa.hstack([spspa.csr_matrix((len(ind_free), nx+neq+nineq)),
-                          spspa.eye(nx).tocsr()[ind_free]]),
-            spspa.hstack([spspa.eye(nx).tocsr()[ind_lb],
-                          spspa.csr_matrix((len(ind_lb), neq+nineq+nx))]),
-            spspa.hstack([spspa.eye(nx).tocsr()[ind_ub],
-                          spspa.csr_matrix((len(ind_ub), neq+nineq+nx))]),
+                          spspa.csr_matrix((len(ind_act), self.problem.neq +
+                                            self.problem.nineq +
+                                            self.problem.nx))]),
+            spspa.hstack([spspa.csr_matrix((len(ind_inact),
+                                            self.problem.nx+self.problem.neq)),
+                          spspa.eye(self.problem.nineq).tocsr()[ind_inact],
+                          spspa.csr_matrix((len(ind_inact),
+                                            self.problem.nx))]),
+            spspa.hstack([spspa.csr_matrix((len(ind_free), self.problem.nx +
+                                            self.problem.neq +
+                                            self.problem.nineq)),
+                          spspa.eye(self.problem.nx).tocsr()[ind_free]]),
+            spspa.hstack([spspa.eye(self.problem.nx).tocsr()[ind_lb],
+                          spspa.csr_matrix((len(ind_lb), self.problem.neq +
+                                            self.problem.nineq +
+                                            self.problem.nx))]),
+            spspa.hstack([spspa.eye(self.problem.nx).tocsr()[ind_ub],
+                          spspa.csr_matrix((len(ind_ub), self.problem.neq +
+                                            self.problem.nineq +
+                                            self.problem.nx))]),
             ]).tocsr()
         rhs = np.hstack([-self.problem.c, self.problem.beq,
                         self.problem.bineq[ind_act],
@@ -196,13 +223,17 @@ class OSQP(object):
             return solution
 
         # Check if the above solution satisfies constraints
-        pol_x = pol_sol[:nx]
-        pol_dual_eq = pol_sol[nx:nx+neq]
-        pol_dual_ineq = pol_sol[nx+neq:nx+neq+nineq]
-        pol_dual_lb = np.zeros(nx)
-        pol_dual_lb[ind_lb] = -pol_sol[nx+neq+nineq:][ind_lb]
-        pol_dual_ub = np.zeros(nx)
-        pol_dual_ub[ind_ub] = pol_sol[nx+neq+nineq:][ind_ub]
+        pol_x = pol_sol[:self.problem.nx]
+        pol_dual_eq = pol_sol[self.problem.nx:self.problem.nx+self.problem.neq]
+        pol_dual_ineq = pol_sol[self.problem.nx+self.problem.neq:
+                                self.problem.nx + self.problem.neq +
+                                self.problem.nineq]
+        pol_dual_lb = np.zeros(self.problem.nx)
+        pol_dual_lb[ind_lb] = -pol_sol[self.problem.nx + self.problem.neq +
+                                       self.problem.nineq:][ind_lb]
+        pol_dual_ub = np.zeros(self.problem.nx)
+        pol_dual_ub[ind_ub] = pol_sol[self.problem.nx + self.problem.neq +
+                                      self.problem.nineq:][ind_ub]
         if all(pol_x > self.problem.lb - self.options.polish_tol) and\
                 all(pol_x < self.problem.ub + self.options.polish_tol)\
                 and all(pol_dual_ineq > -self.options.polish_tol)\
@@ -237,42 +268,46 @@ class OSQP(object):
         # Choose splitting based on the options
         if self.options.splitting == 1:
             solution = self.solve_splitting1()
+        elif self.options.splitting == 2:
+            solution = self.solve_splitting2()
+        else:
+            assert False, "Unknown splitting"
 
         return solution
 
     def solve_splitting1(self):
         """"
         Solve splitting problem with splitting of the form (after introducing
-        slack variables for inequalities
+        slack variables for inequalities)
             minimize	1/2 x' Q x + c'x
-            subject to	 [A] x - [0]z = [b]
-                         [I]     [I]    [0]
-                         z in C
+            subject to	[A] x - [0]z = [b]
+                        [I]     [I]    [0]
+                        z in C
         where C is a set of lower and upper bounds
         """
 
         # Start timer
         t = time.time()
 
-        # Get dimensions
-        nx = self.problem.c.shape[0]
-        neq = self.problem.beq.shape[0]
-        nineq = self.problem.bineq.shape[0]
-        nvar = nx + nineq  # Num of vars in standard form: x and s variables
+        # Number of variables and constraints in standard form
+        nvar = self.problem.nx + self.problem.nineq     # (x,s)
+        nconstr = self.problem.neq + self.problem.nineq
 
         # Form compact (c) matrices for standard QP from:
         #       minimize	1/2 z' Qc z + cc'z
         #       subject to	Ac z == bc
         #                   z \in Z
         Qc = spspa.block_diag((self.problem.Q,
-                              spspa.csc_matrix((nineq, nineq))))
-        cc = np.append(self.problem.c, np.zeros(nineq))
+                               spspa.csc_matrix((self.problem.nineq,
+                                                 self.problem.nineq))))
+        cc = np.append(self.problem.c, np.zeros(self.problem.nineq))
         Ac = spspa.vstack([spspa.hstack([self.problem.Aeq,
-                                        spspa.csc_matrix((neq, nineq))]),
+                                        spspa.csc_matrix(
+                                            (self.problem.neq,
+                                             self.problem.nineq))]),
                            spspa.hstack([self.problem.Aineq,
-                                        spspa.eye(nineq)])])
+                                        spspa.eye(self.problem.nineq)])])
         bc = np.append(self.problem.beq, self.problem.bineq)
-        M = neq + nineq
 
         # Scaling (s): Normalize rows of Ac.
         if self.options.scaling:
@@ -296,12 +331,12 @@ class OSQP(object):
         # Factorize KKT matrix using LU decomposition (for now)
         KKT = spspa.vstack([
             spspa.hstack([Qc + self.options.rho * spspa.eye(nvar), As.T]),
-            spspa.hstack([As, -1. / self.options.rho * spspa.eye(M)])])
+            spspa.hstack([As, -1. / self.options.rho * spspa.eye(nconstr)])])
         luKKT = spalinalg.splu(KKT.tocsc())  # Perform sparse LU factorization
 
         # Set initial conditions to zero
         z = np.zeros(nvar)
-        u = np.zeros(M + nvar)
+        u = np.zeros(nconstr + nvar)
 
         print "Splitting QP Solver"
         print "-------------------\n"
@@ -313,29 +348,28 @@ class OSQP(object):
         for i in range(self.options.max_iter):
 
             # Update RHS of KKT system
-            qtemp = -cc + \
-                self.options.rho * (z + As.T.dot(bs) - As.T.dot(u[:M]) - u[M:])
-            qbar = np.append(qtemp, np.zeros(nineq + neq))
+            qtemp = -cc + self.options.rho * \
+                    (z + As.T.dot(bs) - As.T.dot(u[:nconstr]) - u[nconstr:])
+            qbar = np.append(qtemp, np.zeros(nconstr))
 
             # x update
             x = luKKT.solve(qbar)[:nvar]  # Select first nvar elements
             # z update
             z_old = z
             z = self.project(self.options.alpha*x +
-                             (1.-self.options.alpha)*z_old + u[neq + nineq:],
-                             self.problem.lb, self.problem.ub)
+                             (1.-self.options.alpha)*z_old + u[nconstr:])
             # u update
-            u = u + self.options.alpha*np.append(As.dot(x), x) - \
-                np.append(np.zeros(neq + nineq),
-                          z - (1.-self.options.alpha)*z_old) - \
-                self.options.alpha*np.append(bs, np.zeros(nvar))
+            u = u + self.options.alpha * np.append(As.dot(x), x) \
+                - np.append(np.zeros(nconstr),
+                            z - (1.-self.options.alpha)*z_old) \
+                - self.options.alpha*np.append(bs, np.zeros(nvar))
 
             # Compute primal and dual residuals
             resid_prim = spla.norm(np.append(Ac.dot(x) - bc, x - z))  # eq
             resid_dual = self.options.rho*spla.norm(z - z_old)
 
             # Check the stopping criterion
-            eps_prim = (neq + nineq + nvar) * self.options.eps_abs \
+            eps_prim = (nconstr + nvar) * self.options.eps_abs \
                 + self.options.eps_rel * \
                 np.max([spla.norm(Ac.dot(x)) + spla.norm(x),
                         spla.norm(z),
@@ -343,11 +377,11 @@ class OSQP(object):
 
             eps_dual = nvar * self.options.eps_abs + \
                 self.options.eps_rel * self.options.rho * \
-                (spla.norm(As.T.dot(u[:M])) + spla.norm(u[M:]))
+                (spla.norm(As.T.dot(u[:nconstr])) + spla.norm(u[nconstr:]))
             if resid_prim <= eps_prim and resid_dual <= eps_dual:
                 # Print the progress in last iterations
                 if self.options.print_level > 1:
-                    xtemp = z[:nx]
+                    xtemp = z[:self.problem.nx]
                     f = .5 * np.dot(xtemp.T, self.problem.Q.dot(xtemp)) + \
                         self.problem.c.T.dot(xtemp)
                     print "%4s | \t%7.2f\t    %8.4f\t    %8.4f" \
@@ -361,7 +395,7 @@ class OSQP(object):
                         (np.mod(i + 1,
                          np.floor(np.float(self.options.max_iter)/20.0)) == 0)\
                         | (self.options.print_level == 3):
-                            xtemp = z[:nx]
+                            xtemp = z[:self.problem.nx]
                             f = .5*np.dot(xtemp.T, self.problem.Q.dot(xtemp))\
                                 + self.problem.c.T.dot(xtemp)
                             print "%4s | \t%7.2f\t    %8.4f\t    %8.4f" \
@@ -375,22 +409,22 @@ class OSQP(object):
 
         # Rescale (r) dual variables
         if self.options.scaling:
-            dual_vars = self.options.rho * u[:neq + nineq] / scaler
+            dual_vars = self.options.rho * u[:nconstr] / scaler
         else:
-            dual_vars = self.options.rho * u[:neq + nineq]
+            dual_vars = self.options.rho * u[:nconstr]
 
         # TODO: Scale Qc matrix as well
 
         # TODO: What is a status of the obtained solution?
 
         # Recover primal solution
-        x = z[:nx]
+        x = z[:self.problem.nx]
         objval = .5 * np.dot(x, self.problem.Q.dot(x)) + \
             np.dot(self.problem.c, x)
 
         # Recover dual solution
-        sol_dual_eq = dual_vars[:neq]
-        sol_dual_ineq = dual_vars[neq:]
+        sol_dual_eq = dual_vars[:self.problem.neq]
+        sol_dual_ineq = dual_vars[self.problem.neq:]
         stat_cond_resid = self.problem.Q.dot(x) + self.problem.c + \
             self.problem.Aeq.T.dot(sol_dual_eq) + \
             self.problem.Aineq.T.dot(sol_dual_ineq)
@@ -414,3 +448,15 @@ class OSQP(object):
 
         # Return solution
         return solution
+
+    def solve_splitting2(self):
+        """"
+        Solve splitting problem with splitting of the form (after introducing
+        slack variables for both equality and inequality constraints)
+            minimize	1/2 x' Q x + c'x  + I_{Ax=b}(x) + I_C(z)
+            subject to	x == z
+        where C = [lb, ub]^nx x {0}^neq x R+^nineq
+        """
+
+        # Start timer
+        t = time.time()
