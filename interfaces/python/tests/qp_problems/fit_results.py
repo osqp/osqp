a84eb0c4450031db05034e8b36814f88dc3239d8
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-03-27 15:07:21 +0100

    Added colormap plot behavior.

--- interfaces/python/tests/qp_problems/fit_results.py
+++ interfaces/python/tests/qp_problems/fit_results.py
@@ -1,9 +1,23 @@
 from __future__ import print_function
+
+# For plotting
 import matplotlib as mpl
 # mpl.use('Agg')  # For plotting on remote server
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.interpolate import griddata
+plt.rc('axes', labelsize=18)    # fontsize of the x and y labels
+plt.rc('axes', titlesize=18)   # fontsize of the tick labels
+plt.rc('xtick', labelsize=15)   # fontsize of the tick labels
+plt.rc('ytick', labelsize=15)   # fontsize of the tick labels
+plt.rc('legend', fontsize=15)   # legend fontsize
+plt.rc('text', usetex=True)     # use latex
+plt.rc('font', family='serif')
+
+# Numerics
 import numpy as np
+
+# Dataframes
 import pandas as pd
 from tqdm import tqdm
 
@@ -17,95 +31,104 @@ from tqdm import tqdm
 # # # Define candidate function to be fit
 # # def func_iter(x, c0, c1, c2, c3, c4):
 # #
-# #     return c0*np.power(x[0], c1*x[3] + c2*x[4])*np.power(x[1], c3)*np.power(x[2], c4)
+# #     return c0*np.power(x[0], c1*x[3] + c2*x[4])* \
+# #                        np.power(x[1], c3)*np.power(x[2], c4)
 
 
-def get_rho_and_tr_ratio(df):
+# def get_rho_and_tr_ratio(df):
+#     """
+#     Get best rho and ratio of traces tr(P)/tr(A'A)
+#     """
+#     # Get best parameters
+#     df_best = get_best_params(df)
+#
+#
+#     # get values
+#     trP = df_best['trP']
+#     froA = df_best['froA']
+#     trAtA = froA.values * froA.values
+#
+#
+#     # Compute ratio
+#     trP_over_trAtA = trP / trAtA
+#
+#     # IT IS HAPPENING HERE!!!!
+#     # import ipdb; ipdb.set_trace()
+#
+#     # Get return dataframe
+#     df_return = df_best.loc[:, ['seed', 'n', 'm', 'rho', 'trP', 'froA']]
+#     df_return.loc[:, 'trAtA'] = pd.Series(trAtA,
+#                                           index=df_return.index)
+#     df_return.loc[:, 'n_over_m'] = pd.Series(df_best['n']/df_best['m'],
+#                                                        index=df_return.index)
+#     df_return.loc[:, 'trP_over_trAtA'] = pd.Series(trP_over_trAtA,
+#                                                    index=df_return.index)
+#
+#
+#     # df_return['trAtA'] = trAtA
+#     # df_return['trP_over_trAtA'] = trP_over_trAtA
+#
+#     # # Return new dataframe
+#     # df_return = pd.DataFrame({'seed': [df_best['seed']],
+#     #                           'trP': [df_best['trP']],
+#     #                           'froA': [df_best['froA']],
+#     #                           'trAtA': [df_best['froA'] ** 2],
+#     #                           'trP_over_trAtA': [trP_over_trAtA]})
+#
+#     # import ipdb; ipdb.set_trace()
+#
+#     # return new dataframe
+#     return df_return
+
+
+# def get_best_params(df):
+#     """
+#     Transform weighted frame into another frame with best parameters
+#     """
+#     # Get best parameters
+#     df_best = df.loc[df['e'] == 1.]
+#
+#     # Get highest sigma
+#     max_sigma = df_best['sigma'].max()
+#
+#     # Get best row
+#     df_best = df_best.loc[(df_best['sigma'] == max_sigma)]
+#
+#     if len(df_best) > 1:  # If multiple values choose one with min alpha
+#         min_alpha = df_best['alpha'].min()
+#         df_best = df_best.loc[(df_best['alpha'] == min_alpha)]
+#
+#     if len(df_best) > 1:  # If multiple values chose one with min rho
+#         # Choose min rho element
+#         rho_best = df_best['rho'].min()
+#
+#         if rho_best > 5:
+#             import ipdb; ipdb.set_trace()
+#         # rho_closest_to_min = np.abs(df_best['rho'] - min_rho).min()
+#         df_best = df_best.loc[(df_best['rho'] == rho_best)]
+#         # import ipdb; ipdb.set_trace()
+#         # df_best = df_best.iloc[[idx]]
+#
+#
+#     # import ipdb; ipdb.set_trace()
+#
+#     return df_best
+
+
+def get_efficiency(df):
     """
-    Get best rho and ratio of traces tr(P)/tr(A'A)
+    Compute sample efficiency using their number of iterations related
+    to the min one
     """
-    # Get best parameters
-    df_best = get_best_params(df)
+    df.loc[:, 'e'] = df['iter'].min() / df['iter']
+    return df
 
 
-    # get values
-    trP = df_best['trP']
-    froA = df_best['froA']
-    trAtA = froA.values * froA.values
-
-
-    # Compute ratio
-    trP_over_trAtA = trP / trAtA
-
-    # IT IS HAPPENING HERE!!!!
-    # import ipdb; ipdb.set_trace()
-
-    # Get return dataframe
-    df_return = df_best.loc[:, ['seed', 'n', 'm', 'rho', 'trP', 'froA']]
-    df_return.loc[:, 'trAtA'] = pd.Series(trAtA,
-                                          index=df_return.index)
-    df_return.loc[:, 'n_over_m'] = pd.Series(df_best['n']/df_best['m'],
-                                                       index=df_return.index)
-    df_return.loc[:, 'trP_over_trAtA'] = pd.Series(trP_over_trAtA,
-                                                   index=df_return.index)
-
-
-    # df_return['trAtA'] = trAtA
-    # df_return['trP_over_trAtA'] = trP_over_trAtA
-
-    # # Return new dataframe
-    # df_return = pd.DataFrame({'seed': [df_best['seed']],
-    #                           'trP': [df_best['trP']],
-    #                           'froA': [df_best['froA']],
-    #                           'trAtA': [df_best['froA'] ** 2],
-    #                           'trP_over_trAtA': [trP_over_trAtA]})
-
-    # import ipdb; ipdb.set_trace()
-
-    # return new dataframe
-    return df_return
-
-
-
-def get_best_params(df):
+def get_ratio(df):
     """
-    Transform weighted frame into another frame with best parameters
+    Get ratio tr(P)/tr(A'A) for the dataframe
     """
-    # Get best parameters
-    df_best = df.loc[df['w'] == 1.]
-
-    # Get highest sigma
-    max_sigma = df_best['sigma'].max()
-
-    # Get best row
-    df_best = df_best.loc[(df_best['sigma'] == max_sigma)]
-
-    if len(df_best) > 1:  # If multiple values choose one with min alpha
-        min_alpha = df_best['alpha'].min()
-        df_best = df_best.loc[(df_best['alpha'] == min_alpha)]
-
-    if len(df_best) > 1:  # If multiple values chose one with min rho
-        # Choose min rho element
-        rho_best = df_best['rho'].min()
-
-        if rho_best > 5:
-            import ipdb; ipdb.set_trace()
-        # rho_closest_to_min = np.abs(df_best['rho'] - min_rho).min()
-        df_best = df_best.loc[(df_best['rho'] == rho_best)]
-        # import ipdb; ipdb.set_trace()
-        # df_best = df_best.iloc[[idx]]
-
-
-    # import ipdb; ipdb.set_trace()
-
-    return df_best
-
-
-def weight_by_iter(df):
-    """
-    Weight sample using their number of iterations related to the min one
-    """
-    df.loc[:, 'w'] = df['iter'].min() / df['iter']
+    df.loc[:, 'trPovertrAtA'] = df['trP'] / (df['froA'] * df['froA'])
     return df
 
 
@@ -119,13 +142,13 @@ def save_plot(df, name):
 
     # Get best iteration values (there are many) and pick first pair sigma and alpha
     if name is not 'sigma':
-        test_sigma = df.loc[(df['w'] == 1.)].sigma.values[-1]
+        test_sigma = df.loc[(df['e'] == 1.)].sigma.values[-1]
         location &= (df['sigma'] == test_sigma)
     if name is not 'alpha':
-        test_alpha = df.loc[(df['w'] == 1.)].alpha.values[-1]
+        test_alpha = df.loc[(df['e'] == 1.)].alpha.values[-1]
         location &= (df['alpha'] == test_alpha)
     if name is not 'rho':
-        test_rho = df.loc[(df['w'] == 1.)].rho.values[-1]
+        test_rho = df.loc[(df['e'] == 1.)].rho.values[-1]
         location &= (df['rho'] == test_rho)
 
     # Get test case in specified location
@@ -147,7 +170,7 @@ def save_plot(df, name):
     ax = plt.gca()
     if name is 'rho':
         ax.set_xscale('log')
-    plt.scatter(test_case[name], test_case['w'])
+    plt.scatter(test_case[name], test_case['e'])
     ax.set_ylabel('weight')
     ax.set_xlabel(name)
     plt.grid()
@@ -155,6 +178,15 @@ def save_plot(df, name):
 
     plt.tight_layout()
     plt.savefig('figures/%s.pdf' % name)
+
+
+def get_grid_data(x, y, z, resX=100, resY=100):
+    "Convert 3 column data to matplotlib grid"
+    xi = np.linspace(min(x), max(x), resX)
+    yi = np.linspace(min(y), max(y), resY)
+    zi = griddata((x, y), z, (xi[None, :], yi[:, None]), method='linear')
+    # X, Y = np.meshgrid(xi, yi)
+    return xi, yi, zi
 
 
 # Main function
@@ -170,8 +202,9 @@ if __name__ == '__main__':
                     ignore_index=True)
 
     # res = pd.read_csv('results/results_full.csv')
-    res = res.loc[(res['iter'] < 2499)]  # Select problems not saturated at max number of iterations
 
+    # Select problems not saturated at max number of iterations
+    res = res.loc[(res['iter'] < 2499)]
 
     # Problem headings
     # headings = ['n', 'm', 'name', 'seed']
@@ -180,14 +213,14 @@ if __name__ == '__main__':
     # problems = res.groupby(headings)
     # n_problems = len(problems.groups)
 
-    # Assign weights to samples
+    # Assign efficienct to samples
     problems = res.groupby(['seed'])
-    res_w = problems.apply(weight_by_iter)
-    problems_w = res_w.groupby(['seed'])
+    res_e = problems.apply(get_efficiency)
+    problems_e = res_e.groupby(['seed'])
 
-    '''
-    Plot behavior for fixed sigma and alpha and changing rho
-    '''
+    # '''
+    # Plot behavior for fixed sigma and alpha and changing rho
+    # '''
     # test_name = (50.0, 60.0, 'svm', 3076953921.0)
     # test_name = (50.0, 60.0, 'svm', 107769053.0)
     # test_name = (40.0, 40.0, 'lasso', 685148778.0)
@@ -208,52 +241,63 @@ if __name__ == '__main__':
     # froA2 = froA * froA
 
 
-    '''
-    Plot behavior of optimal rho compared to tr(P)/tr(A'A)
-    '''
-
-    # Group by seed (different randomly generated problem)
-    # problems = res_w.groupby(['seed'])
-
-    # Get first 20 groups
+    # '''
+    # Plot behavior of optimal rho compared to tr(P)/tr(A'A)
+    # '''
 
     # Compute best rho and ratio tr(P)/tr(A'A) for each group
-    # counter = 0
-    rho_and_tr_ratio = pd.DataFrame()
-    for name, group in tqdm(problems_w):
-        # if counter < 30000:
-            # print(name)
-            # if counter == 0:
-                # rho_and_tr_ratio = get_rho_and_tr_ratio(group)
-            # else:
-        rho_and_tr_ratio = \
-            rho_and_tr_ratio.append(get_rho_and_tr_ratio(group))
-        # counter += 1
-
-    # rho_and_tr_ratio_df = pd.concat(rho_and_tr_ratio)
+    # rho_and_tr_ratio = pd.DataFrame()
+    # for name, group in tqdm(problems_w):
+    #     rho_and_tr_ratio = \
+    #         rho_and_tr_ratio.append(get_rho_and_tr_ratio(group))
 
 
     # Plot
-    # x = np.linspace(0.0002, 0.05)
-    # y = 16 * x
-    plt.figure()
-    ax = plt.gca()
-    plt.scatter(rho_and_tr_ratio['trP_over_trAtA'],
-                rho_and_tr_ratio['rho'])
-    # plt.plot(x, y)
-    # ax.set_yscale('log')
-    ax.set_ylabel('rho')
-    ax.set_xlabel('trP_over_trAtA')
-    # ax.set_xlim(-.3, .3)
-    plt.grid()
+    # plt.figure()
+    # ax = plt.gca()
+    # plt.scatter(rho_and_tr_ratio['trP_over_trAtA'],
+    #             rho_and_tr_ratio['rho'])
+    # # ax.set_yscale('log')
+    # ax.set_ylabel('rho')
+    # ax.set_xlabel('trP_over_trAtA')
+    # plt.grid()
+    # plt.show(block=False)
+    # plt.savefig('behavior.pdf')
+
+    '''
+    Create contour plot from 3D scatter plot with rho, ratio, efficiency
+    '''
+    # Get ratio for each group
+    res_e = problems_e.apply(get_ratio)
+
+    # Get grid data
+    xi, yi, zi = get_grid_data(res_e['trPovertrAtA'], res_e['rho'], res_e['e'])
+
+    # Plot contour lines
+    levels = [0., 0.25, 0.5, 0.75, 0.9, 0.95, 1.]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.contour(xi, yi, zi, levels)
+    plt.contourf(xi, yi, zi, levels)
+    ax.set_ylabel(r'$\rho$')
+    ax.set_xlabel(r'$\frac{{\rm tr}(P)}{{\rm tr}(A^{T}A)}$')
+    ax.set_title(r'Efficiency $e$')
+    plt.colorbar()
+    plt.tight_layout()
     plt.show(block=False)
     plt.savefig('behavior.pdf')
 
+    # Get grid data
+    # X, Y, Z = get_grid_data(res_e['rho'], res_e['trPovertrAtA'], res_e['e'])
+    # plt.contourf(X, Y, Z)
 
-    '''
-    Create 3D scatter plot with rho, rato, efficiency
-    '''
 
+
+    # Plot
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.scatter(res_e['rho'], res_e['trPovertrAtA'], res_e['e'])
+    # plt.show(block=False)
 
     # # Plot
     # # x = np.linspace(0.0002, 0.05)
