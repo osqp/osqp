94caf537531ff2c354eb738371e1ce6e4dfc8a68
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-03-29 15:38:25 +0100

    Fixed unittests matlab

--- interfaces/matlab/unittests/warm_start_tests.m
+++ interfaces/matlab/unittests/warm_start_tests.m
@@ -24,6 +24,7 @@ classdef warm_start_tests < matlab.unittest.TestCase
             testCase.options.eps_abs = 1e-04;
             testCase.options.eps_rel = 1e-04;
             testCase.options.early_terminate_interval = 1;
+            testCase.options.auto_rho = 0;
 
             % Setup tolerance
             testCase.tol = 1e-04;
