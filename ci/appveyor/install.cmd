5d115dd5366f9ae02ebd4a8d4ef15468628070c5
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-03-31 19:20:52 +0100

    Added .cmd scripts for appveyor

---
+++ ci/appveyor/install.cmd
@@ -0,0 +1,14 @@
+:: Activate test environment anaconda
+set PATH=%MINICONDA%;%MINICONDA%\\Scripts;%PATH%
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda create -q -n test-environment python=%PYTHON_VERSION% numpy scipy nose
+activate test-environment
+
+
+:: Compile osqp
+mkdir build
+cd build
+cmake -G "MinGW Makefiles" ..
+cmake --build .
---
+++ ci/appveyor/script.cmd
@@ -0,0 +1,2 @@
+:: Perform tests
+%APPVEYOR_BUILD_FOLDER%\build\out\osqp_tester_direct.exe
