d3dc02b6fc6c6e5a164e788596cca70ea052d720
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-04-03 11:46:58 +0100

    Echo in appveyor

--- ci/appveyor/install.cmd
+++ ci/appveyor/install.cmd
@@ -1,3 +1,5 @@
+@echo on
+
 :: Activate test environment anaconda
 set PATH=%MINICONDA%;%MINICONDA%\\Scripts;%PATH%
 conda config --set always_yes yes --set changeps1 no
@@ -6,9 +8,11 @@ conda info -a
 conda create -q -n test-environment python=%PYTHON_VERSION% numpy scipy nose
 activate test-environment
 
-
 :: Compile osqp
 mkdir build
 cd build
 cmake -G"%GENERATOR%" ..
 cmake --build .
+
+
+@echo off
--- ci/appveyor/script.cmd
+++ ci/appveyor/script.cmd
@@ -1,2 +1,7 @@
+@echo on
+
 :: Perform tests
 %APPVEYOR_BUILD_FOLDER%\build\out\osqp_tester_direct.exe
+
+
+@echo off
