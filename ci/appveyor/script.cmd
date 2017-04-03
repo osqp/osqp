92044e314ee441e54327de288c5ca2c65fcb2cc9
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-04-03 19:13:50 +0100

    Changed directory for apveyor files

--- ci/appveyor/script.cmd
+++ ci/appveyor/script.cmd
@@ -1,8 +1,7 @@
 @echo on
 
 :: Perform C Tests
-cd %APPVEYOR_BUILD_FOLDER%\build
-out\osqp_tester_direct.exe
+%APPVEYOR_BUILD_FOLDER%\build\out\osqp_tester_direct.exe
 
 :: Perform Python tests
 cd %APPVEYOR_BUILD_FOLDER%\interfaces\python
