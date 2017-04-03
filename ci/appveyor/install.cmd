4080b75a33f07fd06b5aeddfd0ab0ad0dcbb61a0
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-04-03 15:12:20 +0100

    Try another fix for sh.exe

--- ci/appveyor/install.cmd
+++ ci/appveyor/install.cmd
@@ -5,6 +5,7 @@
 :: (For MinGW make to work correctly sh.exe must NOT be in your path)
 :: http://stackoverflow.com/a/3870338/2288008
 set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
+set PATH=C:\MinGW\bin;%PATH%
 
 :: Workaround for CMake not wanting sh.exe on PATH for MinGW
 REM set PATH=%PATH:C:\Program Files (x86)\Git\bin;=%
