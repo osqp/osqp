453a8b17374c793045c5b6741bb3126b0a58a8de
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-04-05 20:32:22 +0100

    Moved code coverage script

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -22,12 +22,26 @@ echo "Change directory to Travis build ${TRAVIS_BUILD_DIR}"
 cd ${TRAVIS_BUILD_DIR}
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ..
+cmake -G "Unix Makefiles" -DCOVERAGE=ON ..
 make
 
 
 # Test OSQP C
 ${TRAVIS_BUILD_DIR}/build/out/osqp_tester_direct
+
+# Pefrorm code coverage
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    cd ${TRAVIS_BUILD_DIR}/build
+    lcov --directory . --capture -o coverage.info # capture coverage info
+    lcov --remove coverage.info "${TRAVIS_BUILD_DIR}/tests/*" \
+        "${TRAVIS_BUILD_DIR}/lin_sys/direct/suitesparse/amd/*" \
+        "${TRAVIS_BUILD_DIR}/lin_sys/direct/suitesparse/ldl/*" \
+        "${TRAVIS_BUILD_DIR}/lin_sys/direct/suitesparse/SuiteSparse_config*" \
+        -o coverage.info # filter out tests and unnecessary files
+    lcov --list coverage.info # debug before upload
+    coveralls-lcov coverage.info # uploads to coveralls
+fi
+
 
 
 # Test Python interface
