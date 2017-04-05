e990cf40a284dc998dbbaf68db5696ec70d3820b
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-04-05 19:53:38 +0100

    Added conda deployment

---
+++ ci/travis/deploy_anaconda.sh
@@ -0,0 +1,14 @@
+set -e
+
+# Make sure we have the right anaconda path
+export PATH=${DEPS_DIR}/miniconda/bin:$PATH
+cd ${TRAVIS_BUILD_DIR}/interfaces/python/conda_recipe
+
+echo "Creating conda package..."
+conda build osqp --output-dir conda-bld/
+
+echo "Deploying to Anaconda.org..."
+anaconda -t $ANACONDA_TOKEN upload conda-bld/**/osqp-*.tar.bz2
+
+echo "Successfully deployed to Anaconda.org."
+exit 0
