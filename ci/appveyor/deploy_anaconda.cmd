0dc4760636abf437f6fda781a30b13d5c89be55e
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-04-05 20:13:51 +0100

    Fixed anaconda token variable windows

--- ci/appveyor/deploy_anaconda.cmd
+++ ci/appveyor/deploy_anaconda.cmd
@@ -6,5 +6,5 @@ cd %APPVEYOR_BUILD_FOLDER%\interfaces\python\conda_recipe
 conda build osqp --output-dir conda-bld\
 if errorlevel 1 exit /b 1
 
-anaconda -t $ANACONDA_TOKEN upload conda-bld/**/osqp-*.tar.bz2
+anaconda -t %ANACONDA_TOKEN% upload conda-bld/**/osqp-*.tar.bz2
 if errorlevel 1 exit /b 1
