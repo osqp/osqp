f2a33d364684933c6a4796d29b7a7977d4ff2f59
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-04-07 00:14:21 +0100

    Changed deploy for appveyor and travis. Still need to make it working smoothly

--- ci/appveyor/deploy.cmd
+++ ci/appveyor/deploy.cmd
@@ -3,8 +3,11 @@
 
 cd %APPVEYOR_BUILD_FOLDER%\interfaces\python\conda_recipe
 
-conda build --python %PYTHON_VERSION% osqp --output-folder conda-bld\
+call conda build --python %PYTHON_VERSION% osqp --output-folder conda-bld\
 if errorlevel 1 exit /b 1
 
-anaconda -t %ANACONDA_TOKEN% upload conda-bld/**/osqp-*.tar.bz2
+call anaconda -t %ANACONDA_TOKEN% upload conda-bld/**/osqp-*.tar.bz2
 if errorlevel 1 exit /b 1
+
+
+@echo off
