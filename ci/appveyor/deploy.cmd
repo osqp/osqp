1068dcb396861c905805826be3f0472adaa9658f
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-04-06 23:52:56 +0100

    Fixed conda build with python version

--- ci/appveyor/deploy.cmd
+++ ci/appveyor/deploy.cmd
@@ -3,7 +3,7 @@
 
 cd %APPVEYOR_BUILD_FOLDER%\interfaces\python\conda_recipe
 
-conda build osqp --output-folder conda-bld\
+conda build --python %PYTHON_VERSION% osqp --output-folder conda-bld\
 if errorlevel 1 exit /b 1
 
 anaconda -t %ANACONDA_TOKEN% upload conda-bld/**/osqp-*.tar.bz2
--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -1,11 +1,23 @@
 set -e
 
-# Make sure we have the right anaconda path
+
+# Update variables from install
+# CMake
+if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+    export PATH=${DEPS_DIR}/cmake/bin:${PATH}
+fi
+
+# Anaconda
 export PATH=${DEPS_DIR}/miniconda/bin:$PATH
+hash -r
+source activate testenv
+
+
+
 cd ${TRAVIS_BUILD_DIR}/interfaces/python/conda_recipe
 
 echo "Creating conda package..."
-conda build osqp --output-folder conda-bld/
+conda build --python ${PYTHON_VERSION} osqp --output-folder conda-bld/
 
 echo "Deploying to Anaconda.org..."
 anaconda -t $ANACONDA_TOKEN upload conda-bld/**/osqp-*.tar.bz2
