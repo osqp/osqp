94df5891fce0cec5b29f30e11bc2631fcb9d2a37
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-04-06 18:48:47 +0100

    Added matlab interface upload script

---
+++ interfaces/matlab/package/distribute.py
@@ -0,0 +1,73 @@
+import os
+import zipfile
+from platform import system
+import osqp  # Need python OSQP module to run
+import shutil as sh
+from subprocess import call
+
+
+def zipdir(path):
+
+    zipf = zipfile.ZipFile('%s.zip' % path, 'w', zipfile.ZIP_DEFLATED)
+
+    # zipf is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            zipf.write(os.path.join(root, file))
+
+    zipf.close()
+
+
+if __name__ == '__main__':
+
+    # Get oeprative system
+    if system() == 'Windows':
+        platform = 'windows'
+        matlab_ext = 'mexw64'
+    elif system() == 'Linux':
+        platform = 'linux'
+        matlab_ext = 'mexa64'
+    elif system() == 'Darwin':
+        platform = 'mac'
+        matlab_ext = 'mexmaci64'
+
+    # Get OSQP version
+    m = osqp.OSQP()
+    version = m.version()
+
+    # Get package name
+    package_name = "osqp-%s-matlab-%s" % (version, platform)
+
+    # Create build directory
+    if os.path.exists(package_name):
+        sh.rmtree(package_name)
+    os.makedirs(package_name)
+
+    # Copy folders
+    folders_to_copy = ['codegen', 'unittests']
+    for folder_name in folders_to_copy:
+        sh.copytree(os.path.join('..', folder_name),
+                    os.path.join(package_name, folder_name))
+
+    # Copy interface files
+    files_to_copy = ['osqp_mex.%s' % matlab_ext,
+                     'osqp.m']
+
+    for file_name in files_to_copy:
+        sh.copy(os.path.join('..', file_name),
+                os.path.join(package_name))
+
+    # Create zip file
+    zipdir(package_name)
+
+    # Upload on github
+    print("We are ready to upload the package to GitHub")
+    gh_token = raw_input("GitHub token: ")
+
+    call(['github-release', 'upload',
+          '--user', 'oxfordcontrol',
+          '--security-token', gh_token,
+          '--repo', 'oxfordcontrol/osqp',
+          '--tag', 'v%s' % version,
+          '--name', 'Matlab interface %s' % platform,
+          '--file', package_name + '.zip'])
