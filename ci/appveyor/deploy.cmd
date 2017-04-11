a68fcb637b4796db7ec4b7f9a65ae2fd64831ad7
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-04-11 14:38:37 +0100

    Added pypirc creation

--- ci/appveyor/deploy.cmd
+++ ci/appveyor/deploy.cmd
@@ -9,10 +9,18 @@ call anaconda -t %ANACONDA_TOKEN% upload conda-bld/**/osqp-*.tar.bz2 --user oxfo
 if errorlevel 1 exit /b 1
 
 
+:: Specify account details for PyPI
+echo [distutils]                                  > %USERPROFILE%\\.pypirc
+echo index-servers =                             >> %USERPROFILE%\\.pypirc
+echo     testpypi                                    >> %USERPROFILE%\\.pypirc
+echo [testpypi]                                      >> %USERPROFILE%\\.pypirc
+echo repository=https://testpypi.python.org/pypi     >> %USERPROFILE%\\.pypirc
+echo username=bstellato                          >> %USERPROFILE%\\.pypirc
+echo password=%PYPI_PASSWORD%                    >> %USERPROFILE%\\.pypirc
+
 
 cd %APPVEYOR_BUILD_FOLDER%\interfaces\python
 call activate test-environment
-python setup.py bdist_wheel
-twine upload -u %PYPI_USERNAME% -p %PYPI_PASSWORD% dist/*.whl -r pypitest
+python setup.py bdist_wheel upload
 
 )
--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -26,13 +26,24 @@ anaconda -t $ANACONDA_TOKEN upload conda-bld/**/osqp-*.tar.bz2 --user oxfordcont
 echo "Successfully deployed to Anaconda.org."
 
 
+
+# Setup pypi login
+echo [distutils]                                      > $HOME/.pypirc
+echo index-servers =                                 >> $HOME/.pypirc
+echo     testpypi                                    >> $HOME/.pypirc
+echo [testpypi]                                      >> $HOME/.pypirc
+echo repository=https://testpypi.python.org/pypi     >> $HOME/.pypirc
+echo username=bstellato                              >> $HOME/.pypirc
+echo password=$PYPI_PASSWORD                         >> $HOME/.pypirc
+
+
 cd ${TRAVIS_BUILD_DIR}/interfaces/python
 
-echo "Creating pip package..."
-python setup.py bdist_wheel
+echo "Creating and uploading pip package..."
+python setup.py bdist_wheel upload
 
-echo "Deploying to PyPi..."
-twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD dist/*.whl -r pypitest
+# echo "Deploying to PyPi..."
+# twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD dist/*.whl -r pypitest
 echo "Successfully deployed to Pypi"
 
 
