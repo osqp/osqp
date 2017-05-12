Fixed scaling (iter 100 works well). Now it solves 95 Maros problems in few iterations

interfaces/python/examples/bad_convergence/helicopter_scaling.py
@@ -1,5 +1,5 @@
-# import osqp
-import osqppurepy as osqp
+import osqp
+# import osqppurepy as osqp
 import numpy as np
 import scipy.sparse as spa
 
@@ -11,8 +11,10 @@ with open('./data/%s.pickle' % 'helicopter_scaling', 'rb') as f:
 
 
 # OSQP settings
-osqp_settings = {'verbose': True,
+osqp_settings = {'verbose': False,
                  'scaling': True,
+                 'scaling_iter': 50,
+                 'early_terminate_interval': 1,
                  'auto_rho': True,
                  'rho': 0.1,
                  'polish': False}
@@ -21,4 +23,14 @@ osqp_settings = {'verbose': True,
 model = osqp.OSQP()
 model.setup(problem['P'], problem['q'], problem['A'],
             problem['l'], problem['u'], **osqp_settings)
-model.solve()
+res_osqp = model.solve()
+
+
+
+# Solve with GUROBI
+import mathprogbasepy as mpbpy
+qp = mpbpy.QuadprogProblem(problem['P'], problem['q'], problem['A'],
+                      problem['l'], problem['u'])
+res_gurobi = qp.solve(solver=mpbpy.GUROBI, verbose=False)
+print("GUROBI time = %.4e" % res_gurobi.cputime)
+print("OSQP time = %.4e" % res_osqp.info.run_time)

interfaces/python/modulepurepy/_osqp.py
@@ -325,9 +325,56 @@ class OSQP(object):
             # Regularize components
             KKT2d = KKT2.dot(d)
             # Prevent division by 0
-            d = (n + m)*np.reciprocal(KKT2d + 1e-08)
+            d = (n + m)*np.reciprocal(KKT2d + (n + m) * 1e-06)
             # Limit scaling terms
-            d = np.maximum(np.minimum(d, 1e+03), 1e-03)
+            # d = np.maximum(np.minimum(d, 1e+03), 1e-03)
+
+
+            # DEBUG: Check scaling
+            S_temp = spspa.diags(d)
+            D_temp = spspa.diags(d[:self.work.data.n])
+            E_temp = spspa.diags(d[self.work.data.n:])
+            KKT_temp = S_temp.dot(KKT.dot(S_temp)).todense()
+            P_temp = \
+                D_temp.dot(self.work.data.P.dot(D_temp)).todense()
+            A_temp = \
+                E_temp.dot(self.work.data.A.dot(D_temp)).todense()
+            cond_KKT = np.linalg.cond(KKT_temp)
+            cond_P = np.linalg.cond(P_temp)
+            cond_A = np.linalg.cond(A_temp)
+
+            # Get rato between columns and rows
+            n_plus_m = n + m
+            max_norm_rows = 0.0
+            min_norm_rows = np.inf
+            for j in range(n_plus_m):
+                norm_row_j = np.linalg.norm(KKT_temp[j, :])
+                max_norm_rows = np.maximum(norm_row_j,
+                                           max_norm_rows)
+                min_norm_rows = np.minimum(norm_row_j,
+                                           min_norm_rows)
+
+            max_norm_cols = 0.0
+            min_norm_cols = np.inf
+            for j in range(n_plus_m):
+                norm_col_j = np.linalg.norm(KKT_temp[:, j])
+                max_norm_cols = np.maximum(norm_col_j,
+                                           max_norm_cols)
+                min_norm_cols = np.minimum(norm_col_j,
+                                           min_norm_cols)
+
+            # Compute residuals
+            res_rows = max_norm_rows / min_norm_rows
+            res_cols = max_norm_cols / min_norm_cols
+
+            print("\niter %i" % i)
+            print("cond(KKT) = %.4e" % cond_KKT)
+            print("cond(P) = %.4e" % cond_P)
+            print("cond(A) = %.4e" % cond_A)
+            print("res_rows = %.4e / %.4e = %.4e" %
+                  (max_norm_rows, min_norm_rows, res_rows))
+            print("res_cols = %.4e / %.4e = %.4e" %
+                  (max_norm_cols, min_norm_cols, res_cols))
 
         # Obtain Scaler Matrices
         d = np.power(d, 1./self.work.settings.scaling_norm)