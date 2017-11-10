4f9d057413da2d346f81863f9f63cfcf26db5d7d
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-11-10 10:40:05 +0000

    Bump version: 0.2.0.dev5 → 0.2.0.dev6

--- interfaces/matlab/osqp.m
+++ interfaces/matlab/osqp.m
@@ -1,5 +1,5 @@
 classdef osqp < handle
-    % osqp interface class for OSQP solver v0.2.0.dev5
+    % osqp interface class for OSQP solver v0.2.0.dev6
     % This class provides a complete interface to the C implementation
     % of the OSQP solver.
     %
--- interfaces/python/module/interface.py
+++ interfaces/python/module/interface.py
@@ -1,5 +1,5 @@
 """
-Python interface module for OSQP solver v0.2.0.dev5
+Python interface module for OSQP solver v0.2.0.dev6
 """
 from __future__ import print_function
 from builtins import object
