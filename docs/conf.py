60747a62cc4b6d832a5f86da47a4e07cb2f37262
gbanjac <goran.banjac@eng.ox.ac.uk>
2018-04-20 11:55:11 +0200

    Updated homepages and email addresses in docs

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,8 @@ __version__ = "0.3.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.mathjax', 'breathe']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.mathjax', 'breathe',
+              'sphinxcontrib.email']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
