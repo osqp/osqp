6b8d8dcb1419bbbddf472478b15aacc701382ca1
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2018-06-10 18:02:27 -0400

    Bump version: 0.3.1.dev0 → 0.3.1

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -31,7 +31,7 @@ else
     OS_NAME="linux"
     OS_SHARED_LIB_EXT="so"
 fi
-OSQP_DEPLOY_DIR=osqp-0.3.1.dev0-${OS_NAME}64
+OSQP_DEPLOY_DIR=osqp-0.3.1-${OS_NAME}64
 mkdir $OSQP_DEPLOY_DIR/
 mkdir $OSQP_DEPLOY_DIR/lib
 mkdir $OSQP_DEPLOY_DIR/include
@@ -48,10 +48,10 @@ tar -czvf $OSQP_DEPLOY_DIR.tar.gz  $OSQP_DEPLOY_DIR
 
 
 # Deploy package
-curl -T $OSQP_DEPLOY_DIR.tar.gz -ubstellato:$BINTRAY_API_KEY -H "X-Bintray-Package:OSQP" -H "X-Bintray-Version:0.3.1.dev0" https://api.bintray.com/content/bstellato/generic/OSQP/0.3.1.dev0/
+curl -T $OSQP_DEPLOY_DIR.tar.gz -ubstellato:$BINTRAY_API_KEY -H "X-Bintray-Package:OSQP" -H "X-Bintray-Version:0.3.1" https://api.bintray.com/content/bstellato/generic/OSQP/0.3.1/
 
 # Publish
-curl -X POST -ubstellato:$BINTRAY_API_KEY https://api.bintray.com/content/bstellato/generic/OSQP/0.3.1.dev0/publish
+curl -X POST -ubstellato:$BINTRAY_API_KEY https://api.bintray.com/content/bstellato/generic/OSQP/0.3.1/publish
 
 
 exit 0
