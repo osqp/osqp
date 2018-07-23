5f31ba92ccfbd9d16a3ced81cba13ad978ca0ba7
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2018-07-23 14:28:02 -0400

    Bump version: 0.3.1 → 0.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ import subprocess
 # sys.path.insert(0, os.path.abspath('.'))
 
 # Set version for bumpversion
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 # -- General configuration ------------------------------------------------
 
