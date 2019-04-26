85512922f095b9b03ece5689e1b68b5942e1401b
Nikhil Kalra <nikalra@adobe.com>
2019-04-26 15:58:46 -0700

    Update osqp_tester.c
    Added tests for the custom allocators.

--- tests/osqp_tester.c
+++ tests/osqp_tester.c
@@ -23,12 +23,55 @@
 
 int tests_run = 0;
 
+int custom_malloc_called = 0;
+int custom_free_called = 0;
+int custom_realloc_called = 0;
+
+void* custom_malloc(size_t size)
+{
+  custom_malloc_called++;
+  return malloc(size);
+}
+
+void* custom_calloc(size_t count, size_t size)
+{
+  void* ptr = custom_malloc(count * size);
+  memset(ptr, 0, count * size);
+  return ptr;
+}
+
+void custom_free(void* ptr)
+{
+  custom_free_called++;
+  return free(ptr);
+}
+
+void* custom_realloc(void* ptr, size_t size)
+{
+  custom_realloc_called++;
+  return realloc(ptr, size);
+}
 
 static char* all_tests() {
   mu_run_test(test_lin_alg);
   mu_run_test(test_solve_linsys);
+
+  // set custom allocator
+  OSQPGlobalOptions opts = { 
+    custom_malloc, 
+    custom_calloc, 
+    custom_free, 
+    custom_realloc 
+  };
+  osqp_set_global_options(&opts);
+
   mu_run_test(test_basic_qp);
   mu_run_test(test_basic_qp2);
+  
+  // remove custom allocator
+  memset(&opts, 0, sizeof(OSQPGlobalOptions));
+  osqp_set_global_options(&opts);
+
   mu_run_test(test_non_cvx);
   mu_run_test(test_primal_infeasibility);
   mu_run_test(test_primal_dual_infeasibility);
@@ -47,6 +90,17 @@ int main(void) {
     printf("ALL TESTS PASSED\n");
   }
   printf("Tests run: %d\n", tests_run);
+
+  if ((custom_malloc_called > 0) && (custom_realloc_called > 0) && (custom_free_called > 0)) {
+    printf("Custom malloc called: %d\n", custom_malloc_called);
+    printf("Custom realloc called: %d\n", custom_realloc_called);
+    printf("Custom free called: %d\n", custom_free_called);
+    printf("Custom memory management checks PASSED\n");
+  }
+  else {
+    printf("Custom malloc/realloc/free were not called");
+    result = "Custom malloc/free failed.";
+  }
 
   return result != 0;
 }
