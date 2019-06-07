768bd0933fca35fb6bff9821dbd09758cd350c15
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2019-06-07 13:05:49 -0400

    ci

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -9,30 +9,6 @@ fi
 mkdir ${DEPS_DIR}
 cd ${DEPS_DIR}
 
-# Install CMake
-if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
-    CMAKE_URL="http://www.cmake.org/files/v3.7/cmake-3.7.1-Linux-x86_64.tar.gz"
-else
-    CMAKE_URL="http://www.cmake.org/files/v3.7/cmake-3.7.1-Darwin-x86_64.tar.gz"
-fi
-mkdir cmake && wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-export PATH=${DEPS_DIR}/cmake/bin:${PATH}
-cmake --version
-
-
-# Install lcov and valgrind on linux
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    sudo apt-get update -y
-    sudo apt-get install -y lcov
-    sudo apt-get install -y valgrind
-else if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    brew update
-    brew install lcov
-    fi
-fi
-
-gem install coveralls-lcov
-
 
 # Install Anaconda
 
@@ -50,6 +26,34 @@ conda config --set always_yes yes --set changeps1 no
 conda update --yes -q conda
 conda create -n testenv --yes python=$PYTHON_VERSION numpy scipy future
 source activate testenv
+
+# Install cmake valgrind and lcov
+conda install --yes -c conda-forge cmake valgrind lcov
+
+# # Install CMake
+# if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+#     CMAKE_URL="http://www.cmake.org/files/v3.7/cmake-3.7.1-Linux-x86_64.tar.gz"
+# else
+#     CMAKE_URL="http://www.cmake.org/files/v3.7/cmake-3.7.1-Darwin-x86_64.tar.gz"
+# fi
+# mkdir cmake && wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
+# export PATH=${DEPS_DIR}/cmake/bin:${PATH}
+# cmake --version
+
+# # Install lcov and valgrind on linux
+# if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+#     sudo apt-get update -y
+#     sudo apt-get install -y lcov
+#     sudo apt-get install -y valgrind
+# else if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+#     brew update
+#     brew install lcov
+#     fi
+# fi
+
+gem install coveralls-lcov
+
+
 
 # Add MKL shared libraries to the path
 export MKL_SHARED_LIB_DIR=`python -c 'import numpy.distutils.system_info as sysinfo; print(sysinfo.get_info("mkl")["library_dirs"][0])'`
