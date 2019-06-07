25cb85d3ac8b16361ff019da0d5f059167f313c9
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2019-06-07 10:48:09 -0400

    CI

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -12,12 +12,11 @@ cd ${DEPS_DIR}
 # Install CMake
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
     CMAKE_URL="http://www.cmake.org/files/v3.7/cmake-3.7.1-Linux-x86_64.tar.gz"
-    mkdir cmake && wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-    export PATH=${DEPS_DIR}/cmake/bin:${PATH}
 else
-brew update
-brew upgrade cmake || brew install cmake
+    CMAKE_URL="http://www.cmake.org/files/v3.7/cmake-3.7.1-Darwin-x86_64.tar.gz"
 fi
+mkdir cmake && wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
+export PATH=${DEPS_DIR}/cmake/bin:${PATH}
 cmake --version
 
 
