34336b3dfbc1a84a10e749baf8fd4a56e639de2d
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2019-08-14 17:59:05 -0400

    Removed custom allocator test print. Otherwise CI logs are too long

--- tests/custom_memory/custom_memory.c
+++ tests/custom_memory/custom_memory.c
@@ -9,20 +9,20 @@ long int alloc_counter = 0;
 void* my_malloc(size_t size) {
   void *m = malloc(size);
   alloc_counter++;
-  printf("OSQP allocator  (malloc): %zu bytes, %ld allocations \n",size, alloc_counter);
+  /* printf("OSQP allocator  (malloc): %zu bytes, %ld allocations \n",size, alloc_counter); */
   return m;
 }
 
 void* my_calloc(size_t num, size_t size) {
   void *m = calloc(num, size);
   alloc_counter++;
-  printf("OSQP allocator  (calloc): %zu bytes, %ld allocations \n",num*size, alloc_counter);
+  /* printf("OSQP allocator  (calloc): %zu bytes, %ld allocations \n",num*size, alloc_counter); */
   return m;
 }
 
 void* my_realloc(void *ptr, size_t size) {
   void *m = realloc(ptr,size);
-  printf("OSQP allocator (realloc) : %zu bytes, %ld allocations \n",size, alloc_counter);
+  /* printf("OSQP allocator (realloc) : %zu bytes, %ld allocations \n",size, alloc_counter); */
   return m;
 }
 
@@ -30,6 +30,6 @@ void my_free(void *ptr) {
   if(ptr != NULL){
     free(ptr);
     alloc_counter--;
-    printf("OSQP allocator   (free) : %ld allocations \n", alloc_counter);
+    /* printf("OSQP allocator   (free) : %ld allocations \n", alloc_counter); */
   }
 }
