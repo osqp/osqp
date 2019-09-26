43c71a2fe3aafa0095a124f2d52e9d574c5d0e8a
gbanjac <gbanjac@control.ee.ethz.ch>
2019-09-26 19:08:17 +0200

    Add new files

---
+++ algebra/default/algebra_libs.c
@@ -0,0 +1,13 @@
+
+#include "cuda_handler.h"
+
+CUDA_Handle_t *CUDA_handle;
+
+void algebra_init_libs(void) {
+  CUDA_handle = CUDA_init_libs();
+  cuda_init_libs();
+}
+
+void algebra_free_libs(void) {
+  cuda_free_libs(CUDA_handle);
+}
