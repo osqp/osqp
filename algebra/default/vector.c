53211123736d702187f7a0b54d4a46f9be7a676e
gbanjac <gbanjac@control.ee.ethz.ch>
2019-09-27 08:12:06 +0000

    Compilation works

--- algebra/default/vector.c
+++ algebra/default/vector.c
@@ -5,7 +5,7 @@
 #include "cuda_handler.h"
 //#include "cuda_malloc.h"
 #include "cuda_wrapper.h"
-#include "helper_cuda.h"    /* --> checkCudaErrors */
+//#include "helper_cuda.h"    /* --> checkCudaErrors */
 
 /* VECTOR FUNCTIONS ----------------------------------------------------------*/
 
