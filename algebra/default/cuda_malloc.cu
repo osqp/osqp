62580cc598be4fd5f6eca7ee780a6ad29adacc88
gbanjac <gbanjac@control.ee.ethz.ch>
2019-09-27 08:57:03 +0000

    Typo fix

--- algebra/default/cuda_malloc.cu
+++ algebra/default/cuda_malloc.cu
@@ -32,7 +32,7 @@ void cuda_calloc(void** devPtr, size_t size) {
   checkCudaErrors(c_cudaCalloc(devPtr, size));
 }
 
-void cuda_calloc(void** devPtr, size_t size) {
+void cuda_free(void** devPtr, size_t size) {
   c_cudaFree(devPtr);
 }
 
