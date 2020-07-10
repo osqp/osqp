595afd50dce8633afeeae6aee594e5f7c6c4d13f
gbanjac <gbanjac@control.ee.ethz.ch>
2020-07-10 21:36:45 +0200

    Updated copyright notice

--- algebra/cuda/include/csr_type.h
+++ algebra/cuda/include/csr_type.h
@@ -1,5 +1,6 @@
 /**
- *  Copyright (c) 2019 ETH Zurich, Automatic Control Lab, Michel Schubiger, Goran Banjac.
+ *  Copyright (c) 2019-2020 ETH Zurich, Automatic Control Lab,
+ *  Michel Schubiger, Goran Banjac.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
