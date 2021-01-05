6d6b126fbf55cfd004a5bc1b60d7720f69e7dd5d
gbanjac <gbanjac@control.ee.ethz.ch>
2021-01-05 21:35:39 +0100

    Updated copyright notice

--- algebra/cuda/src/cuda_lin_alg.cu
+++ algebra/cuda/src/cuda_lin_alg.cu
@@ -1,5 +1,5 @@
 /**
- *  Copyright (c) 2019-2020 ETH Zurich, Automatic Control Lab,
+ *  Copyright (c) 2019-2021 ETH Zurich, Automatic Control Lab,
  *  Michel Schubiger, Goran Banjac.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
