73bda30af9cea46d06a512c5dc9953d9531865bd
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2021-01-05 16:58:46 -0500

    Bump version: 0.6.2.dev0 → 0.6.2

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ import subprocess
 # sys.path.insert(0, os.path.abspath('.'))
 
 # Set version for bumpversion
-__version__ = "0.6.2.dev0"
+__version__ = "0.6.2"
 
 # -- General configuration ------------------------------------------------
 
