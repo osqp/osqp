abd4b5e93fd8f2460b0342c4717bdd7d84108783
Ian McInerney <ian.mcinerney@manchester.ac.uk>
2022-06-13 18:18:36 +0100

    Add tests to ensure codegen can't generate nonconvex problems

--- tests/non_cvx/test_non_cvx.h
+++ tests/non_cvx/test_non_cvx.h
@@ -74,3 +74,92 @@ void test_non_cvx_solve()
   clean_problem_non_cvx(data);
   clean_problem_non_cvx_sols_data(sols_data);
 }
+
+#ifdef OSQP_CODEGEN
+void test_non_cvx_codegen()
+{
+  c_int exitflag;
+
+  // Problem settings
+  OSQPSettings *settings = (OSQPSettings *)c_malloc(sizeof(OSQPSettings));
+
+  // Codegen defines
+  OSQPCodegenDefines *defines = (OSQPCodegenDefines *)c_malloc(sizeof(OSQPCodegenDefines));
+
+  // Structures
+  OSQPSolver *solver; // Workspace
+  OSQPTestData *data;      // Data
+  non_cvx_sols_data *sols_data;
+
+
+  // Populate data
+  data = generate_problem_non_cvx();
+  sols_data = generate_problem_non_cvx_sols_data();
+
+
+  // Define Solver settings as default
+  osqp_set_default_settings(settings);
+  settings->verbose = 1;
+  settings->adaptive_rho = 0;
+  settings->sigma = 1e-6;
+
+  // Define codegen settings
+  defines->embedded_mode = 1;    // vector update
+  defines->float_type = 1;       // floats
+  defines->printing_enable = 0;  // no printing
+  defines->profiling_enable = 0; // no timing
+  defines->interrupt_enable = 0; // no interrupts
+
+  // Setup workspace
+  exitflag = osqp_setup(&solver, data->P, data->q,
+                        data->A, data->l, data->u,
+                        data->m, data->n, settings);
+
+  // Setup should fail due to (P + sigma I) having a negative eigenvalue
+  mu_assert("Non Convex codegen: Setup should have failed!",
+            exitflag == OSQP_NONCVX_ERROR);
+
+  exitflag = osqp_codegen(solver, "./", "noncvx_", defines);
+
+  // Codegen should fail due to (P + sigma I) having a negative eigenvalue
+  mu_assert("Non Convex codegen: codegen type 1 should have failed!",
+            exitflag == OSQP_NONCVX_ERROR);
+
+  defines->embedded_mode = 2;    // matrix update
+
+  exitflag = osqp_codegen(solver, "./", "noncvx_", defines);
+
+  // Codegen should fail due to (P + sigma I) having a negative eigenvalue
+  mu_assert("Non Convex codegen: codegen type 2 should have failed!",
+            exitflag == OSQP_NONCVX_ERROR);
+
+
+  osqp_cleanup(solver);
+
+  // Update Solver settings
+  settings->sigma = sols_data->sigma_new;
+
+  // Setup workspace again
+  exitflag = osqp_setup(&solver, data->P, data->q,
+                        data->A, data->l, data->u,
+                        data->m, data->n, settings);
+
+  // Setup should work this time because (P + sigma I) is positive definite
+  mu_assert("Non Convex codegen: Setup error!", exitflag == 0);
+
+  exitflag = osqp_codegen(solver, "./", "noncvx_", defines);
+
+  // Codegen should work since (P + sigma I) is positive definite
+  mu_assert("Non Convex codegen: codegen should have worked!",
+            exitflag == 0);
+
+  // Clean solver
+  osqp_cleanup(solver);
+
+  // Cleanup settings and data
+  c_free(settings);
+  c_free(defines);
+  clean_problem_non_cvx(data);
+  clean_problem_non_cvx_sols_data(sols_data);
+}
+#endif
