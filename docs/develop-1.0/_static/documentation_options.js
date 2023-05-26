86256bd4484db29667f379f078220e36b7878d70
imciner2 <ian.s.mcinerney@ieee.org>
2023-05-26 15:16:23 +0000

    Deploying to gh-pages from @ osqp/osqp@ced27928ab20b55cf88a3933bcc65bc998bf50c8 🚀

--- docs/develop-1.0/_static/documentation_options.js
+++ docs/develop-1.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.6.3',
+    VERSION: '',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
