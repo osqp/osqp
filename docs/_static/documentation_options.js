f517484ca5949042322b7848c90ae3b668d00397
vineetbansal <vineetbansal@protonmail.com>
2023-05-31 18:05:08 +0000

    Deploying to gh-pages from @ osqp/osqp@e5f1e5017c82341ea201247d3bd1fd6254c2665d 🚀

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '',
+    VERSION: 'v1.0.0.beta1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
