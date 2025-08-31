efdf70ca478d62f4f089722f0c0f7a0cb676d8b7
lb3825 <lb3825@princeton.edu>
2025-08-31 17:46:04 -0400

    test

--- src/auxil.c
+++ src/auxil.c
@@ -1509,6 +1509,10 @@ OSQPInt check_termination(OSQPSolver* solver,
   info->prim_integral = info->prim_integral * info->delta_solve_time;
   info->dual_integral = info->dual_integral * info->delta_solve_time;
   info->duality_gap_integral = info->duality_gap_integral * info->delta_solve_time;
+  c_print("info->prim_integral: %f\n", info->prim_integral);
+  c_print("info->dual_integral: %f\n", info->dual_integral);
+  c_print("info->duality_gap_integral: %f\n", info->duality_gap_integral);
+  c_print("info->delta_solve_time: %f\n", info->delta_solve_time);
   info->total_integral += (info->prim_integral + info->dual_integral + info->duality_gap_integral);
   // info->total_integral += (info->prim_integral + info->dual_integral);
   info->run_time_prev = osqp_toc(work->timer);
