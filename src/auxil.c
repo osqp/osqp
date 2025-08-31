151e20baa47a98240d252b55f79314b14821bc4c
lb3825 <lb3825@princeton.edu>
2025-08-31 17:51:33 -0400

    test

--- src/auxil.c
+++ src/auxil.c
@@ -1509,15 +1509,15 @@ OSQPInt check_termination(OSQPSolver* solver,
   info->prim_integral = info->prim_integral * info->delta_solve_time;
   info->dual_integral = info->dual_integral * info->delta_solve_time;
   info->duality_gap_integral = info->duality_gap_integral * info->delta_solve_time;
-  c_print("info->prim_integral: %f\n", info->prim_integral);
-  c_print("info->dual_integral: %f\n", info->dual_integral);
-  c_print("info->duality_gap_integral: %f\n", info->duality_gap_integral);
-  c_print("info->delta_solve_time: %f\n", info->delta_solve_time);
   info->total_integral += (info->prim_integral + info->dual_integral + info->duality_gap_integral);
   // info->total_integral += (info->prim_integral + info->dual_integral);
   info->run_time_prev = osqp_toc(work->timer);
   // c_print("info->delta_solve_time: %f\n", info->delta_solve_time);
-  // c_print("info->total_integral: %f\n", info->total_integral);
+  c_print("info->prim_integral: %e\n", info->prim_integral);
+  c_print("info->dual_integral: %e\n", info->dual_integral);
+  c_print("info->duality_gap_integral: %e\n", info->duality_gap_integral);
+  c_print("info->delta_solve_time: %e\n", info->delta_solve_time);
+  c_print("info->total_integral: %e\n", info->total_integral);
 
   // Compare checks to determine solver status
   if (prim_res_check && dual_res_check && duality_gap_check) {
