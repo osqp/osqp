1ece836c98ae2b1e847e16839c26e7cf38e27c04
lb3825 <lb3825@princeton.edu>
2025-09-05 12:27:10 -0400

    test

--- include/private/glob_opts.h
+++ include/private/glob_opts.h
@@ -67,6 +67,42 @@ extern "C" {
 # endif
 
 
+
+# ifndef c_exp_40
+#  define c_exp_40(x) ({ \
+    OSQPFloat _x_orig = (x); \
+    OSQPFloat _x = c_absval(_x_orig); \
+    OSQPFloat _result = 1.0; \
+    OSQPFloat _term = 1.0; \
+    OSQPInt _max_iter = 40; \
+    OSQPInt _iter; \
+    for (_iter = 1; _iter <= _max_iter; _iter++) { \
+      _term *= _x / _iter; \
+      _result += _term; \
+    } \
+    (_x_orig >= 0) ? _result : (1.0 / _result); \
+  })
+# endif
+
+
+
+# ifndef c_exp_25
+#  define c_exp_25(x) ({ \
+    OSQPFloat _x_orig = (x); \
+    OSQPFloat _x = c_absval(_x_orig); \
+    OSQPFloat _result = 1.0; \
+    OSQPFloat _term = 1.0; \
+    OSQPInt _max_iter = 25; \
+    OSQPInt _iter; \
+    for (_iter = 1; _iter <= _max_iter; _iter++) { \
+      _term *= _x / _iter; \
+      _result += _term; \
+    } \
+    (_x_orig >= 0) ? _result : (1.0 / _result); \
+  })
+# endif
+
+
 /* Use customized functions -----------------------------------------------   */
 
 # if OSQP_EMBEDDED_MODE != 1
