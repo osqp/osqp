32f7224f54f7cc9f3497816244bf7f3e1c1759a4
Vineet Bansal <vineetbansal@protonmail.com>
2025-09-24 10:31:59 -0400

    configuring qdldl before add_subdirectory

--- algebra/_common/lin_sys/qdldl/qdldl.cmake
+++ algebra/_common/lin_sys/qdldl/qdldl.cmake
@@ -1,10 +1,3 @@
-include(FetchContent)
-
-message(STATUS "Fetching/configuring QDLDL solver")
-list(APPEND CMAKE_MESSAGE_INDENT "  ")
-
-add_subdirectory(${OSQP_ALGEBRA_ROOT}/../ext/qdldl ${OSQP_ALGEBRA_ROOT}/../ext/qdldl/build EXCLUDE_FROM_ALL)
-
 # Make QDLDL use the same types as OSQP
 set(QDLDL_FLOAT ${OSQP_USE_FLOAT} CACHE BOOL "QDLDL Float type")
 set(QDLDL_LONG ${OSQP_USE_LONG} CACHE BOOL "QDLDL Integer type")
@@ -12,6 +5,11 @@ set(QDLDL_LONG ${OSQP_USE_LONG} CACHE BOOL "QDLDL Integer type")
 # We only want the object library, so turn off the other library products
 set(QDLDL_BUILD_STATIC_LIB OFF CACHE BOOL "Build QDLDL static library")
 set(QDLDL_BUILD_SHARED_LIB OFF CACHE BOOL "Build QDLDL shared library")
+
+message(STATUS "Configuring QDLDL solver")
+list(APPEND CMAKE_MESSAGE_INDENT "  ")
+
+add_subdirectory(${OSQP_ALGEBRA_ROOT}/../ext/qdldl ${OSQP_ALGEBRA_ROOT}/../ext/qdldl/build EXCLUDE_FROM_ALL)
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
 
